d9cdb9c635fef061ed9f580889d52e21c8fee70d
Turtogtokh Altangerel <keatonturkuush.ta.ta@gmail.com>
2024-06-05 12:16:57 +0200

    update dummy test

--- src/test/java/com/example/springdemo/DummyTests.java
+++ src/test/java/com/example/springdemo/DummyTests.java
@@ -1,8 +1,13 @@
 package com.example.springdemo;
 
+import com.example.springdemo.page_objects.DummyPage;
 import org.testng.annotations.Test;
 
 public class DummyTests extends TestBase {
   @Test
-  public void dummyTest() {}
+  public void dummyTest() {
+    System.out.println("Starting the test");
+    getBean(DummyPage.class).clickOnDummyElement();
+    System.out.println("Finishing the test");
+  }
 }
