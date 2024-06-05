17ca9508f10c9c0e40ccc3cdd9e5e200219adc45
Turtogtokh Altangerel <keatonturkuush.ta.ta@gmail.com>
2024-06-05 12:15:35 +0200

    add timeout to Configuration class

--- src/main/java/com/example/springdemo/Configuration.java
+++ src/main/java/com/example/springdemo/Configuration.java
@@ -1,5 +1,8 @@
 package com.example.springdemo;
 
+import java.time.Duration;
+
 public class Configuration {
   public static final String DRIVER_TYPE = "chrome";
+  public static final Duration DEFAULT_TIMEOUT_DURATION = Duration.ofSeconds(15);
 }
